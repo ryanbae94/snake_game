a3dd83dc661433262ef5880f94de153f8e438e1a
Soohyun1389 <97733366+Soohyun1389@users.noreply.github.com>
2023-06-20 19:51:31 +0900

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -1,32 +1,29 @@
-#include "Game.h"
+#include "SnakeGame.h"
+#include "map.h"
 
-//Map Display Color Setting
-void cell_color_init(){
+void Display::init_coloring(){
   init_color(COLOR_GRAY, 500, 500, 500);
-
-  init_pair(1, COLOR_GRAY, COLOR_GRAY);   //(1)Wall
-  init_pair(2, COLOR_BLACK, COLOR_BLACK); //(2)Immune Wall
-  init_pair(3, COLOR_YELLOW, COLOR_YELLOW); //(3)Snake Head
-  init_pair(4, COLOR_BLUE, COLOR_BLUE);   //(4)Snake Body
-  init_pair(5, COLOR_MAGENTA, COLOR_MAGENTA); //(5)Gate
-  init_pair(6, COLOR_RED, COLOR_RED);   //(6)Poison Item
-  init_pair(7, COLOR_GREEN, COLOR_GREEN); //(7)Growth Item
-  init_pair(8, COLOR_WHITE, COLOR_WHITE); //(8)Blank
-
-  init_pair(9, COLOR_GRAY, COLOR_WHITE); //End
+  init_pair(1, COLOR_WHITE, COLOR_WHITE); //(0)Blank
+  init_pair(2, COLOR_GRAY, COLOR_GRAY);   //(1)Wall
+  init_pair(3, COLOR_BLACK, COLOR_BLACK); //(2)Immune Wall
+  init_pair(4, COLOR_YELLOW, COLOR_YELLOW); //(3)Snake Head
+  init_pair(5, COLOR_BLUE, COLOR_BLUE);   //(4)Snake Body
+  init_pair(6, COLOR_MAGENTA, COLOR_MAGENTA); //(5)Gate
+  init_pair(7, COLOR_RED, COLOR_RED);   //(6)Poison Item
+  init_pair(8, COLOR_GREEN, COLOR_GREEN); //(7)Growth Item
 }
 
-//Map display = win1
-void map_set(){
-  for(int i = 0; i < 25; i++)
+void Display::coloring(int stage_num){
+  for(int i = 0; i < 25; i++){
     for(int j = 0; j < 25; j++){
-      int cell_color = map[stage_level][i][j];
-      if(cell_color == 8) cell_color = 0;
-      wattron(win1, COLOR_PAIR(cell_color));
-      mvprintw(win1, i, j, "0");
-      wattroff(win1, COLOR_PAIR(cell_color));
+      wattron(snake_win, COLOR_PAIR(map[stage_num][i][j] + 1));
+      mvwprintw(snake_win, i, j, "%d", map[stage_num][i][j]);
+      wattroff(snake_win, COLOR_PAIR(map[stage_num][i][j] + 1));
     }
-  wrefresh(win1);
+  }
+  wrefresh(snake_win);
 }
 
-//Board 구현
+void Display::refresh(){
+  wrefresh(snake_win);
+}
